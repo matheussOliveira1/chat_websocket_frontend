971c607ac689c7914b4b8a0c6552b9d162423c2a
matheussOliveira1 <matheus.aoliveira@sptech.school>
2025-02-10 12:43:40 -0300

    feat: tratamento de envio e recebimento de mensagens funcionando (front + back conversando entre si)

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const loginInput = login.querySelector(".login__input");
 const chat = document.querySelector(".chat");
 const chatForm = chat.querySelector(".chat__form");
 const chatInput = chat.querySelector(".chat__input");
+const chatMessages = chat.querySelector(".chat__messages");
 
 const user = {id: "", name: "", color: ""};
 
@@ -19,14 +20,59 @@ const colors = [
     "gold"
 ]
 
-let websocket;
+let ws;
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div");
+    
+    div.classList.add("message--self");
+    
+    div.innerHTML = content;
+    
+    return div; 
+}
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div");
+    const span  = document.createElement("span");
+    
+    div.classList.add("message--other");
+    span.classList.add("message--sender");
+    span.style.color = senderColor;
+    
+    div.appendChild(span);
+
+    span.innerHTML = sender;
+    div.innerHTML += content;
+
+    return div; 
+}
 
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
 
-const handleSubmit = (event) => {
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content} = JSON.parse(data);
+
+    const message = userId == user.id 
+    ? createMessageSelfElement(content) 
+    : createMessageOtherElement(content, userName, userColor);
+
+    chatMessages.appendChild(message);
+
+    scrollScreen();
+}
+
+const handleLogin = (event) => {
     event.preventDefault();
 
     user.id = crypto.randomUUID();
@@ -36,9 +82,26 @@ const handleSubmit = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    ws = new WebSocket("ws://localhost:8080");
+    ws.onmessage = processMessage;
 
     console.log(user);
 }
 
-loginForm.addEventListener("submit", handleSubmit);
+const sendMessage = (event) => {
+    event.preventDefault();
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+
+    ws.send(JSON.stringify(message));
+
+    chatInput.value = "";
+}
+
+loginForm.addEventListener("submit", handleLogin);
+chatForm.addEventListener("submit", sendMessage);
